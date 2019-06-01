10c4e4b11763e979ce8cd04dc33874eea74783ba
aboudicheng <aboudicheng@gmail.com>
2019-06-01 16:53:43 +0300

    Handle error

--- src/App.js
+++ src/App.js
@@ -8,11 +8,10 @@ import {
 } from "react-simple-maps";
 import {
   Select,
-  Button
+  Button,
+  Spin
 } from 'antd';
 import ReactTooltip from 'react-tooltip';
-import languages from './language_codes.json';
-import world from './world-50m.json';
 import axios from 'axios';
 import * as api_endpoints from './constants/api_endpoints';
 
@@ -32,26 +31,28 @@ function App() {
   const [selectedLanguages, setSelectedLanguages] = useState([]);
   const [selectedCountries, setSelectedCountries] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [hoverCountry, setHoverCountry] = useState("");
+  const [languages, setLanguages] = useState([]);
+  const [world, setWorld] = useState([]);
 
   useEffect(() => {
-    console.log(selectedCountries)
-  }, [selectedCountries])
+    fetchLanguages();
+    fetchWorld();
+  }, [])
 
   useEffect(() => {
     setTimeout(() => {
       ReactTooltip.rebuild()
     }, 100)
-  }, [])
+  }, [selectedCountries])
 
-  function handleMove(geography, evt) {
-    const x = evt.clientX
-    const y = evt.clientY + window.pageYOffset
-    setHoverCountry(geography.properties.name);
-    console.log(geography.properties.name)
+  async function fetchLanguages() {
+    const { data } = await axios.get(api_endpoints.languages);
+    setLanguages(data);
   }
-  function handleLeave() {
-    setHoverCountry("");
+
+  async function fetchWorld() {
+    const { data } = await axios.get(api_endpoints.world);
+    setWorld(data);
   }
 
   function handleLangChange(value) {
@@ -63,19 +64,24 @@ function App() {
       const promises = [];
       selectedLanguages.forEach(lang => {
         const promise = new Promise(async (resolve, reject) => {
-          setLoading(true)
-          const { data } = await axios.get(`${api_endpoints.lang}/${lang}`);
-          const countries = []
-          data.forEach(country => {
-            countries.push(country['alpha3Code']);
-          });
-          resolve(countries);
+          try {
+            setLoading(true)
+            const { data } = await axios.get(`${api_endpoints.lang}/${lang}`);
+            const countries = []
+            data.forEach(country => {
+              countries.push(country['alpha3Code']);
+            });
+            resolve(countries);
+          }
+          catch (e) {
+            resolve();
+            throw e;
+          }
         });
         promises.push(promise);
       });
 
       Promise.all(promises).then(values => {
-        console.log(values)
         setSelectedCountries(values.flat())
         setLoading(false);
       })
@@ -87,7 +93,7 @@ function App() {
       <div style={{ textAlign: "center", margin: "25px 15px" }}>
         <Select
           mode="multiple"
-          placeholder="Choose a language"
+          placeholder="Choose any language"
           style={{ width: 400, marginRight: 15 }}
           onChange={handleLangChange}
           filterOption={(input, option) =>
@@ -100,23 +106,20 @@ function App() {
         </Select>
         <Button type="primary" onClick={handleSearch}>Go</Button>
       </div>
-      <div style={{ textAlign: "center" }}>
-        {!loading &&
+
+      {loading ? <div style={{ height: 450, display: "flex", justifyContent: "center", alignItems: "center" }}><Spin size="large" /></div>
+        : <div style={{ textAlign: "center" }}>
           <ComposableMap>
             <ZoomableGroup disablePanning>
               <Geographies geography={world}>
-                {(geographies, projection) => {
-                  return geographies.map((geography, i) => {
+                {(geographies, projection) =>
+                  geographies.map((geography, i) => {
                     return (
                       <Geography
                         key={i}
                         geography={geography}
                         projection={projection}
-                        onMouseMove={handleMove}
-                        onMouseLeave={handleLeave}
                         data-tip={geography.properties.name}
-                        //data-tip={hoverCountry}
-                        //data-for={'countryTooltip'}
                         style={{
                           default: {
                             fill: selectedCountries.includes(geography.id) ? "#00c42a" : "#ECEFF1",
@@ -141,12 +144,11 @@ function App() {
                     )
                   })
                 }
-                }
               </Geographies>
             </ZoomableGroup>
           </ComposableMap>
-        }
-      </div>
+        </div>
+      }
       <ReactTooltip />
     </div>
   );
--- src/constants/api_endpoints.js
+++ src/constants/api_endpoints.js
@@ -1,2 +1,4 @@
 export const name = "https://restcountries.eu/rest/v2/name";
 export const lang = "https://restcountries.eu/rest/v2/lang";
+export const languages = `${process.env.PUBLIC_URL}/data/language_codes.json`;
+export const world = `${process.env.PUBLIC_URL}/data/world-50m.json`;
