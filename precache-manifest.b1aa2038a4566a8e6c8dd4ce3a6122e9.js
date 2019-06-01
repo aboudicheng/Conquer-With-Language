bfd64307c5943290620597409033918392067e98
aboudicheng <aboudicheng@gmail.com>
2019-06-01 17:43:21 +0300

    Updates

---
+++ precache-manifest.b1aa2038a4566a8e6c8dd4ce3a6122e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3912860a4234fab7670a538211dbad92",
+    "url": "/Conquer-With-Language/index.html"
+  },
+  {
+    "revision": "c7d7279e37faa4e72fb8",
+    "url": "/Conquer-With-Language/static/css/2.7ff19518.chunk.css"
+  },
+  {
+    "revision": "c7d7279e37faa4e72fb8",
+    "url": "/Conquer-With-Language/static/js/2.d683c8d9.chunk.js"
+  },
+  {
+    "revision": "3fa947a873d112170171",
+    "url": "/Conquer-With-Language/static/js/main.dce71e49.chunk.js"
+  },
+  {
+    "revision": "90a4b0acb36177fa1c5c",
+    "url": "/Conquer-With-Language/static/js/runtime~main.1ed54f0b.js"
+  }
+]);
