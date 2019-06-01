aa58398ac22ebd740159e55ac6456bfa426e1899
aboudicheng <aboudicheng@gmail.com>
2019-06-01 16:22:11 +0300

    Add simple functionalities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,146 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState, useEffect } from "react";
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+} from "react-simple-maps";
+import {
+  Select,
+  Button
+} from 'antd';
+import ReactTooltip from 'react-tooltip';
+import languages from './language_codes.json';
+import world from './world-50m.json';
+import axios from 'axios';
+import * as api_endpoints from './constants/api_endpoints';
+
+const { Option } = Select;
+
+const wrapperStyles = {
+  width: "100%",
+  maxWidth: 1280,
+  margin: "0 auto",
+  fontFamily: "Roboto, sans-serif",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+}
 
 function App() {
+  const [selectedLanguages, setSelectedLanguages] = useState([]);
+  const [selectedCountries, setSelectedCountries] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [hoverCountry, setHoverCountry] = useState("");
+
+  useEffect(() => {
+    console.log(selectedCountries)
+  }, [selectedCountries])
+
+  function handleMove(geography, evt) {
+    const x = evt.clientX
+    const y = evt.clientY + window.pageYOffset
+    setHoverCountry(geography.properties.name);
+    console.log(geography.properties.name)
+  }
+  function handleLeave() {
+    setHoverCountry("");
+  }
+
+  function handleLangChange(value) {
+    setSelectedLanguages(value);
+  }
+
+  function handleSearch() {
+    if (selectedLanguages) {
+      const promises = [];
+      selectedLanguages.forEach(lang => {
+        const promise = new Promise(async (resolve, reject) => {
+          setLoading(true)
+          const { data } = await axios.get(`${api_endpoints.lang}/${lang}`);
+          const countries = []
+          data.forEach(country => {
+            countries.push(country['alpha3Code']);
+          });
+          resolve(countries);
+        });
+        promises.push(promise);
+      });
+
+      Promise.all(promises).then(values => {
+        console.log(values)
+        setSelectedCountries(values.flat())
+        setLoading(false);
+      })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={wrapperStyles}>
+      <div style={{ textAlign: "center", margin: "25px 15px" }}>
+        <Select
+          mode="multiple"
+          placeholder="Choose a language"
+          style={{ width: 400, marginRight: 15 }}
+          onChange={handleLangChange}
+          filterOption={(input, option) =>
+            option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
         >
-          Learn React
-        </a>
-      </header>
+          {languages.map((lang, i) =>
+            <Option value={lang["alpha2"]} key={`${lang["alpha2"]}-${i}`}>{lang["English"]}</Option>
+          )}
+        </Select>
+        <Button type="primary" onClick={handleSearch}>Go</Button>
+      </div>
+      <div style={{ textAlign: "center" }}>
+        {!loading &&
+          <ComposableMap>
+            <ZoomableGroup>
+              <Geographies geography={world}>
+                {(geographies, projection) => {
+                  return geographies.map((geography, i) => {
+                    return (
+                      <Geography
+                        key={i}
+                        geography={geography}
+                        projection={projection}
+                        onMouseMove={handleMove}
+                        onMouseLeave={handleLeave}
+                        data-tip={hoverCountry}
+                        data-for={'countryTooltip'}
+                        style={{
+                          default: {
+                            fill: selectedCountries.includes(geography.id) ? "#00c42a" : "#ECEFF1",
+                            stroke: "#607D8B",
+                            strokeWidth: 0.75,
+                            outline: "none",
+                          },
+                          hover: {
+                            fill: "#607D8B",
+                            stroke: "#607D8B",
+                            strokeWidth: 0.75,
+                            outline: "none",
+                          },
+                          pressed: {
+                            fill: "#FF5722",
+                            stroke: "#607D8B",
+                            strokeWidth: 0.75,
+                            outline: "none",
+                          },
+                        }}
+                      />
+                    )
+                  })
+                }
+                }
+              </Geographies>
+            </ZoomableGroup>
+          </ComposableMap>
+        }
+      </div>
+      <ReactTooltip id='countryTooltip' />
     </div>
   );
 }
---
+++ src/constants/api_endpoints.js
@@ -0,0 +1,2 @@
+export const name = "https://restcountries.eu/rest/v2/name";
+export const lang = "https://restcountries.eu/rest/v2/lang";
